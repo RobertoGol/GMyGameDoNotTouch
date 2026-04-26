9d97d5b3e0e5e12a209295c3627000e7f1f6c3c7
RobertoGol <gol89264@gmail.com>
2026-04-26 19:45:33 +0300

    Revert "Test v 3.82"
    This reverts commit 48757f33a127e933dcc48d8933ab01bc90a8fc60.

--- build_finish_msvc/external/glfw/src/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/Projects/Game_Project/external/glfw/src
-# Build directory: D:/Projects/Game_Project/build_finish_msvc/external/glfw/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
