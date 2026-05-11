135cb306af89ca33027d04db4bef0451fc00ae39
RobertoGol <gol89264@gmail.com>
2026-05-11 18:46:43 +0300

    Update manual in Log branch

--- include/WorldEvents.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "GameRuntime.hpp"
-
-namespace bunker {
-
-void ProcessScriptedWorldEvents(const World& world, const PlayerState& player, SessionProfile& profile, GameState& gameState);
-
-}  // namespace bunker
